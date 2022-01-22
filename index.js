2525c05a1362c180e67e01ce77ffbbb693186f1f
EmissarKir <61598599+EmissarKir@users.noreply.github.com>
2022-01-22 23:33:55 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,68 @@
+const yargs = require("yargs");
+const pkg = require("./package.json");
+const {
+  addNote,
+  printNotes,
+  removeNote,
+  updateNote,
+} = require("./notes.controller");
+
+yargs.version(pkg.version);
+
+yargs.command({
+  command: "add",
+  describe: "Add new note to list",
+  builder: {
+    title: {
+      type: "string",
+      describe: "Note title",
+      demandOption: true,
+    },
+  },
+  handler({ title }) {
+    addNote(title);
+  },
+});
+
+yargs.command({
+  command: "list",
+  describe: "Print all notes",
+  async handler() {
+    printNotes();
+  },
+});
+yargs.command({
+  command: "remove",
+  describe: "Remove note by id",
+  builder: {
+    id: {
+      type: "string",
+      describe: "Remove title",
+      demandOption: true,
+    },
+  },
+  async handler({ id }) {
+    removeNote(id);
+  },
+});
+yargs.command({
+  command: "edit",
+  describe: "Edit note by id or title",
+  builder: {
+    id: {
+      type: "string",
+      describe: "Edit id",
+      demandOption: true,
+    },
+    title: {
+      type: "string",
+      describe: "Edit title",
+      demandOption: true,
+    },
+  },
+  async handler({ id, title }) {
+    updateNote(id, title);
+  },
+});
+
+yargs.parse();
